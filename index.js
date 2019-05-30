10cf289ccd8c515b594058abdee710226fcd60c9
widows26 <51136929+widows26@users.noreply.github.com>
2019-05-30 23:11:19 +0200

    Jeszcze więcej tekstów

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const korwin1 = [
     "Ja chciałem państwu przypomnieć, że",
     "Niech państwo nie mają złudzeń:",
     "Powiedzmy to wyraźnie:",
-    "Mnie uczono (za komuny to było), że"
+    "Mnie uczono (za komuny to było), że powinna być normalność. A",
+    "Proszę państwa,",
+    "Proszę mi uwierzyć,"
+    "Proszę państwa! Przecież to jest rzeczą niewyobrażalną, że"
+    "Szanowni państwo, ja się urodziłem w normalnych czasach, kiedy panowała normalność. A dzisiaj"
 ];
 
 const korwin2 = [
@@ -17,9 +21,10 @@ const korwin2 = [
     "ci wszyscy (tfu!) demokraci",
     "agenci bezpieki",
     "feminazistki",
-    "marksiści, leniniści, maoiści",
+    "marksiści, leniniści i maoiści",
     "agenci Kiszczaka",
     "unijni biurokraci",
+    "ci ekoterroryści",
     "socjaliści - wszystko jedno - zieloni, czerwoni czy niebiescy"
 ];
 
@@ -33,7 +38,8 @@ const korwin3 = [
     "w ramach postępu",
     "na skutek umów w Magdalence i przy Okrągłym Stole",
     "w ramach walki z tak zwanym globalnym ociepleniem",
-    "sponsorowani przez Amerykanów"
+    "sponsorowani przez Amerykanów",
+    "za państwa pieniądze"
 ];
 
 const korwin4 = [
@@ -45,31 +51,35 @@ const korwin4 = [
     "nie dopuszczają prawicy do władzy",
     "uczą dzieci homoseksualizmu",
     "próbują nas uciszyć",
-    "zatrudniają coraz więcej urzędników"
+    "zatrudniają coraz więcej urzędników",
+    "fałszują wybory"
 ];
 
 const korwin5 = [
-    "bo dzięki temu mogą kraść",
-    "bo dostają za to pieniądze",
-    "bo tak się uczy w państwowej szkole",
-    "bo bez tego (tfu!) demokracja nie może istnieć",
-    "bo głupich jest więcej niż mądrych",
-    "bo chcą stworzyć raj na ziemi",
-    "bo chcą niszczyć cywilizacje białego człowieka",
-    "ponieważ są socjalistami"
+    "bo dzięki temu mogą kraść,",
+    "bo dostają za to pieniądze,",
+    "bo tak się uczy w państwowej szkole,",
+    "bo bez tego (tfu!) demokracja nie może istnieć,",
+    "bo głupich jest więcej niż mądrych,",
+    "bo chcą stworzyć raj na ziemi,",
+    "bo chcą niszczyć cywilizacje białego człowieka,",
+    "ponieważ są socjalistami,",
+    "żeby wprowadzić ustrój komunistyczny,"
+    "aby pozbawić Polskę suwerenności,"
 ];
 
 const korwin6 = [
-    "przez kolejne kadencje",
     "o czym się nie mówi",
     "i właśnie dlatego Europa umiera",
     "ale przyjdą muzułmanie i zrobią porządek",
-    "- tak samo zresztą jak za Hitlera",
-    "- proszę zobaczyć, co się dzieje na Zachodzie, jeśli mi państwo nie wierzą",
+    "tak samo zresztą jak za Hitlera",
+    "proszę zobaczyć, co się dzieje na Zachodzie, jeśli mi państwo nie wierzą",
     "co lat temu sto nikomu nie przyszłoby nawet do głowy",
     "dlatego musimy zabić, kurwa, tych ludzi!",
-    "- ja o tym mówię od trzydziestu pięciu lat",
-    "- dlatego Unię Europejską musimy zniszczyć"
+    "i ja o tym mówię od trzydziestu pięciu lat",
+    "dlatego Unię Europejską należy zniszczyć",
+    "co w normalnym kraju nie byłoby możliwe",
+    "dlatego w następnych wyoborach zdobędziemy co najmniej 20%"
 ];
 
 module.exports = function() {
